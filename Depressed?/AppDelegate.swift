946c3b56dad77c37cfb0c214f36eccdfdda3f8f2
Akash Desai <kash650@gmail.com>
2018-10-12 18:31:42 -0700

    changed api keys path

--- Depressed?/AppDelegate.swift
+++ Depressed?/AppDelegate.swift
@@ -21,31 +21,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 }
-
-struct SesameProperties {
-    static let file: SesameProperties = {
-        guard let path = Bundle.main.path(forResource: "APIKeys", ofType: "plist"),
-            let apiKeys = NSDictionary(contentsOfFile: path) as? [String: Any],
-            let sesameKeys = apiKeys["Sesame"] as? [String: String],
-            let sesameProperties = SesameProperties(dict: sesameKeys)
-            else { fatalError() }
-        return sesameProperties
-    }()
-
-    let appId: String
-    let auth: String
-    let versionId: String
-    let userId: String
-
-    init?(dict: [String: Any]) {
-        guard let appId = dict["appId"] as? String, !appId.isEmpty,
-            let auth = dict["auth"] as? String, !auth.isEmpty,
-            let versionId = dict["versionId"] as? String, !versionId.isEmpty,
-            let userId = dict["userId"] as? String, !userId.isEmpty
-            else { return nil }
-        self.appId = appId
-        self.auth = auth
-        self.versionId = versionId
-        self.userId = userId
-    }
-}
